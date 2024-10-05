90fb1ac9787db75eb6877112c37ea31381ed56ab
Ayaj Husain <ayajhusain51@gmail.com>
2024-10-05 21:26:38 +0530

    part1 Started

---
+++ part1/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const friends = ['Ayaj','zain']
+  const combine = friends.join('')
+  return (
+    <>
+      <p>{friends}</p>
+      <p>{combine}</p>
+    </>
+    
+  )
+}
+
+export default App
