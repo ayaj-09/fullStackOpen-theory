30381c36bfae6a43599fdabbf1e74b5745e3f4eb
Ayaj Husain <ayajhusain51@gmail.com>
2024-10-07 17:32:46 +0530

    part1Ends

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,49 @@
+import {useState} from 'react'
+
+const History = ({allClick}) =>{
+  if(allClick.length==0){
+    return (
+      <p>The app is used by clicking buttons</p>
+    )
+  }
+  else{
+    return(
+      <p>{allClick}</p>
+    )
+  }
+}
+
+const Button = ({onSmash,text}) =><button onClick={onSmash}>{text}</button>
+
+
 const App = () => {
-  const friends = ['Ayaj','zain']
-  const combine = friends.join('')
+
+  const [left,setLeft] = useState(0)
+  const [right,setRight] = useState(0)
+  const [allClick,setAllClick] = useState([])
+  const [total,setTotal] = useState(0)
+
+  const handleLeftClick = () =>{
+    setAllClick([...allClick,'L'])
+    setLeft(left+1)
+    setTotal(left+1+right)
+  }
+  const handleRightClick = () =>{
+    setAllClick([...allClick,'R'])
+    setRight(right+1)
+    setTotal(left+right+1)
+  }
+
   return (
-    <>
-      <p>{friends}</p>
-      <p>{combine}</p>
-    </>
-    
+    <div>
+      {left}
+      <Button onSmash={handleLeftClick} text = 'left'/>
+      <Button onSmash = {handleRightClick} text = 'right'/>
+      {right}
+      <br/>
+      <History allClick={allClick}/>
+      <p>total {total}</p>
+    </div>
   )
 }
 
