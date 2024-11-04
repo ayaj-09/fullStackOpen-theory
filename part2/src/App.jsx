33cb1b1524f54f00058c8800ad9a1cc5e2236e9f
Ayaj Husain <ayajhusain51@gmail.com>
2024-11-04 12:20:03 +0530

    'part2Started'

---
+++ part2/src/App.jsx
@@ -0,0 +1,13 @@
+import Note from './components/Note'
+
+const App = ({notes}) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(n=><Note key = {n.id} note = {n}/>)}
+      </ul> 
+    </div>
+  )
+}
+export default App
